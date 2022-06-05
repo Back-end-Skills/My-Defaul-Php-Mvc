524c4ad6da8cd936761ba264460fa572f371c4c6
Isaque Menezes <isaque-menezes@hotmail.com>
2022-06-05 11:17:55 -0300

    config detalhes

--- index.php
+++ index.php
@@ -5,31 +5,13 @@
     require "core/config/config.php";              
     require "vendor/autoload.php";  
 
-    //Teste Unitário para namespace Classes
-    use Classes\ClassTest;
-    $obj = new ClassTest();
+    echo "Minha constante config DIRPAGE para caminho da url <br> ". DIRPAGE;
     echo "<hr>";
 
-    //Teste Unitário para namespace Models
-    use Models\ModelTest;
-    $obj_models = new ModelTest();
+    echo "Minha constante config DIRREQ para caminho físico<br> ". DIRREQ;
     echo "<hr>";
 
-    //Teste Unitário para namespace Traits
-    //http://localhost/github/My-Defaul-Php-Mvc/string0
-    use Traits\TraitParseUrl;
-
-    class ClassTraitsTest{
-        use TraitParseUrl;
-        public function __construct(){
-            echo "Teste Unitário namespace Traits.Tudo Ok!<br>";
-            var_dump($this->parseUrl('0'));
-            //Output string(7) "string0" 
-        }
-    }
-
-    $obj = new ClassTraitsTest();
-    echo "<hr>";
+    echo "Minha constante config DOMAIN para caminho <br>". DOMAIN;
 
   
 
